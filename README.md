44cd583ee9c982f251fe836126279ee22dacbcc6
Sonter <sonterkub@gmail.com>
2025-01-06 14:28:00 +0300

    Fix some todo's

--- README.md
+++ README.md
@@ -8,10 +8,10 @@ git clone https://github.com/rodentous/micro-nvim.git "$config"/nvim
 
 ### TODO:
 
-- [ ] remap ESC to do nothing (no normal mode fr)
+- [X] remap ESC to do nothing (no normal mode fr)
 - [ ] make ctrl+arrows move cursor one word left or right
 - [ ] when arrow-left on first position move to previous line last position and vice versa
-- [ ] no relative line numbers
+- [X] no relative line numbers
 - [ ] alt+arrow-up/down move line up/down
-- [ ] add commandline on Ctrl-B
-- [ ] tab 4 spaces
+- [X] add commandline on Ctrl-B
+- [X] tab 4 spaces
--- init.lua
+++ init.lua
@@ -7,9 +7,14 @@ vim.opt.autochdir = true
 vim.cmd.colorscheme "catppuccin-frappe"
 vim.cmd.cnoreabbrev "qq qa!"
 
--- use relative line numbers
+-- <Tab> = 4 spaces
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+-- don't use relative line numbers
 vim.opt.number = true
-vim.opt.relativenumber = true
+vim.opt.relativenumber = false
 
 -- don't wrap lines
 vim.opt.wrap = false
@@ -37,15 +42,13 @@ require'lspconfig'.pylsp.setup{
 --  See `:help hlsearch`
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
-vim.keymap.set('n', '<A-.>', "<cmd>tabnext<CR>", { desc = "Next tab" })
-vim.keymap.set('n', '<A-,>', "<cmd>tabprevious<CR>", { desc = "Previous tab" })
-vim.keymap.set('n', 'yd', "<cmd>co.<CR>", { desc = "Duplicate line down" })
-vim.keymap.set('n', 'yp', "<cmd>co.<CR>", { desc = "Duplicate line down" })
-
-
 --- MICRO CONFIGURATION ---
 -- Auto insert
 vim.cmd("startinsert")
+
+vim.keymap.set('n', '<A-.>', "<cmd>tabnext<CR>", { desc = "Next tab" })
+vim.keymap.set('n', '<A-,>', "<cmd>tabprevious<CR>", { desc = "Previous tab" })
+
 
 vim.keymap.set('i', '<C-s>', "<cmd>w<CR>", { desc = "Save file", noremap = true })
 vim.keymap.set('i', '<C-k>', "<cmd>.,.d_<CR>", { desc = "Remove line", noremap = true })
@@ -58,11 +61,16 @@ vim.keymap.set('n', '<BS>', "Xi", { desc = "Backspace", noremap = true })
 vim.keymap.set('v', '<BS>', "Xi", { desc = "Backspace", noremap = true })
 vim.keymap.set('n', '<Esc>', "i", { desc = "Insert mode", noremap = true })
 
+-- no normal mode
+vim.keymap.set('i', '<Esc>', '<nop>')
+
 vim.keymap.set('i', '<C-q>', "<cmd>qa<CR>", { desc = "Quit entierly", noremap = true })
 vim.keymap.set('i', '<C-w>', "<cmd>q<CR>", { desc = "Close current file", noremap = true })
 
 vim.keymap.set('i', '<A-.>', "<cmd>tabnext<CR>", { desc = "Next tab", noremap = true })
 vim.keymap.set('i', '<A-,>', "<cmd>tabprevious<CR>", { desc = "Previous tab", noremap = true })
+
+vim.keymap.set('i', '<C-b>', "<cmd>terminal<CR>", { desc = "Terminal", noremap = true })
 
 
 vim.cmd("inoremap <C-Z> <C-O>u")
@@ -91,6 +99,4 @@ vim.opt.keymodel = 'startsel,stopsel'
 -- vim.keymap.set('i', '<C-y>', "<C-O><cmd>redo<CR>", { desc = "Redo", noremap = true })
 --
 vim.keymap.set('i', '<C-d>', "<cmd>co.<CR>", { desc = "Duplicate line down", noremap = true })
-
-
 
