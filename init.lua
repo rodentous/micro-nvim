4c55058f6f8989165f990884feba68f8e9ac6f72
Sonter <sonterkub@gmail.com>
2025-01-07 10:41:33 +0300

    Fix deleting words, opening files with Ctrl-O, add paste to selection, fix find highlights

--- init.lua
+++ init.lua
@@ -38,6 +38,7 @@ require'lspconfig'.pylsp.setup{
   }
 }
 
+-- no normal mode
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
 vim.keymap.set({'i', 'n'}, '<Esc>', '<cmd>nohlsearch<CR>', { noremap = true })
@@ -47,22 +48,19 @@ vim.keymap.set({'i', 'n'}, '<Esc>', '<cmd>nohlsearch<CR>', { noremap = true })
 -- Auto insert
 vim.cmd("startinsert")
 
--- no normal mode
-vim.keymap.set('i', '<Esc>', '<nop>')
-
-vim.keymap.set('n', '<A-.>', "<cmd>tabnext<CR>", { desc = "Next tab" })
-vim.keymap.set('n', '<A-,>', "<cmd>tabprevious<CR>", { desc = "Previous tab" })
-
 vim.keymap.set('i', '<C-s>', "<cmd>w<CR>", { desc = "Save file", noremap = true })
 vim.keymap.set('i', '<C-k>', "<cmd>.,.d_<CR>", { desc = "Remove line", noremap = true })
 vim.keymap.set('i', '<C-x>', "<C-O>dd", { desc = "Cut line", noremap = true })
-vim.keymap.set('v', '<C-x>', "xi", { desc = "Cut", noremap = true })
-vim.keymap.set('v', '<C-c>', "yi", { desc = "Copy", noremap = true })
+vim.keymap.set('v', '<C-x>', "x", { desc = "Cut", noremap = true })
+vim.keymap.set('v', '<C-c>', "y", { desc = "Copy", noremap = true })
+vim.keymap.set('v', '<C-v>', '"_xp', { desc = "Paste", noremap = true })
 vim.keymap.set('i', '<C-c>', "<C-O>yy", { desc = "Copy", noremap = true })
-vim.keymap.set('i', '<C-v>', "<C-O>pi", { desc = "Paste", noremap = true })
+vim.keymap.set('i', '<C-v>', "<C-O>p", { desc = "Paste", noremap = true })
 vim.keymap.set('i', '<C-a>', "<C-O>G<C-O>$<C-O>vgg0i", { desc = "Select all", noremap = true })
+
 vim.keymap.set('i', '<C-f>', "<C-O>/", { desc = "Find", noremap = true })
--- vim.keymap.set('i', '<C-o>', "<C-O>:e ", { desc = "Open", noremap = true })
+vim.keymap.set('i', '<C-o>', "<C-O>:tabedit ", { desc = "Open", noremap = false })
+vim.keymap.set('i', '<C-A-O>', "<C-O>", { desc = "Temporarily use normal mode", noremap = false })
 
 
 vim.keymap.set('i', '<C-S-Up>', "<cmd>m -2<CR>", { desc = "Move line up", noremap = true })
@@ -73,11 +71,11 @@ vim.keymap.set('i', '<A-Left>', "<C-O>0", { desc = "Move to the beginning of the
 vim.keymap.set('i', '<A-Right>', "<C-O>$", { desc = "Move to the end of the current line", noremap = true })
 
 
-vim.keymap.set('i', '<C-_>', "<C-O>gcc", { desc = "Comment", remap = true })
+vim.keymap.set('i', '<C-_>', "<C-A-O>gcc", { desc = "Comment", remap = true })
 vim.keymap.set('v', '<C-_>', "gc", { desc = "Comment", remap = true })
 
-vim.keymap.set('i', '\b', '<C-O>"_db', { desc = "Delete word", noremap = true })
-vim.keymap.set('i', '<C-Del>', '<C-O>"_dw', { desc = "Delete word to the right", noremap = true })
+vim.keymap.set('i', '\b', '<C-\\><C-O>"_db', { desc = "Delete word", noremap = true })
+vim.keymap.set('i', '<C-Del>', '<C-\\><C-A-O>"_dw', { desc = "Delete word to the right", noremap = true })
 
 vim.keymap.set('n', '<BS>', '"_X', { desc = "Backspace", noremap = true })
 vim.keymap.set('v', '<BS>', '"_x', { desc = "Backspace", noremap = true })
